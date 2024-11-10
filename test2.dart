069b8800429196f8e61afeb007132f6d321c9359
IBRAR ALI <codewithbara0@gmail.com>
2024-11-10 03:20:56 -0800

    Add files via upload

---
+++ test2.dart
@@ -0,0 +1,16 @@
+void main (){
+
+    //Enter the area of traiangle
+
+   double base = 134;
+   double  height = 66;
+
+
+  // formula to find out the area of traingle
+   double area = 0.5*base*height;
+
+  print("Area of traingle $area");
+
+    
+
+}
